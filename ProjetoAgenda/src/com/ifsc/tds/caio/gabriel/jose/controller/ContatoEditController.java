01675376b460f8470db7406e0d370edb3d72e063
José Carlos Moura <124727238+zecamoura@users.noreply.github.com>
2023-05-31 16:06:02 -0300

    çlkpoujpo[k
    saaefreg

--- ProjetoAgenda/src/com/ifsc/tds/caio/gabriel/jose/controller/ContatoEditController.java
+++ ProjetoAgenda/src/com/ifsc/tds/caio/gabriel/jose/controller/ContatoEditController.java
@@ -1,5 +1,48 @@
 package com.ifsc.tds.caio.gabriel.jose.controller;
 
-public class ContatoEditController {
+	import javafx.fxml.FXML;
+	import javafx.scene.control.Button;
+	import javafx.scene.control.Label;
+	import javafx.scene.control.TextField;
+	import javafx.scene.layout.AnchorPane;
+	import javafx.scene.layout.GridPane;
 
-}
+	public class ContatoEditController {
+
+	    @FXML
+	    private AnchorPane pnlPrincipal;
+
+	    @FXML
+	    private GridPane pnlDados;
+
+	    @FXML
+	    private Label lblNome;
+
+	    @FXML
+	    private Label lblTelefone;
+
+	    @FXML
+	    private Label lblEmail;
+
+	    @FXML
+	    private TextField txtNome;
+
+	    @FXML
+	    private TextField txtTelefone;
+
+	    @FXML
+	    private TextField txtEmail;
+
+	    @FXML
+	    private GridPane pnlButton;
+
+	    @FXML
+	    private Button btnOk;
+
+	    @FXML
+	    private Button btnCnacela;
+
+	}
+
+
+
