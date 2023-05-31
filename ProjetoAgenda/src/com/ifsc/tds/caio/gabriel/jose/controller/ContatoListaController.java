d1b69286570c361cdfc36f8edc4ee08fbd52c323
costinha32 <caio.ic12@aluno.ifsc.edu.br>
2023-05-31 15:20:29 -0300

    Adicionei o fxml na classe controller

--- ProjetoAgenda/src/com/ifsc/tds/caio/gabriel/jose/controller/ContatoListaController.java
+++ ProjetoAgenda/src/com/ifsc/tds/caio/gabriel/jose/controller/ContatoListaController.java
@@ -1,5 +1,100 @@
 package com.ifsc.tds.caio.gabriel.jose.controller;
 
-public class ContatoListaController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.Label;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
+
+public class ContatoListaController  implements Initializable{
+
+    @FXML
+    private AnchorPane pnlPrincipal;
+
+    @FXML
+    private SplitPane pnlDivisao;
+
+    @FXML
+    private AnchorPane pnlEsquerda;
+
+    @FXML
+    private TableView<?> tbvCodigoNome;
+
+    @FXML
+    private TableColumn<?, ?> tbcCodigo;
+
+    @FXML
+    private TableColumn<?, ?> tbcNome;
+
+    @FXML
+    private AnchorPane pnlDireita;
+
+    @FXML
+    private Label lblDetalhes;
+
+    @FXML
+    private GridPane pnlDetalhes;
+
+    @FXML
+    private Label lblNome;
+
+    @FXML
+    private Label lblTelefone;
+
+    @FXML
+    private Label lblEtiquetaValor;
+
+    @FXML
+    private ButtonBar barBotoes;
+
+    @FXML
+    private Button btnInclur;
+
+    @FXML
+    private Tooltip tlpIncluir;
+
+    @FXML
+    private Button btnEditar;
+
+    @FXML
+    private Tooltip tlpEditar;
+
+    @FXML
+    private Button btnExcluir;
+
+    @FXML
+    private Tooltip tlpExcluir;
+
+    @FXML
+    void onClickBtnEditar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickBtnExcluir(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickBtnIncluir(ActionEvent event) {
+
+    }
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
