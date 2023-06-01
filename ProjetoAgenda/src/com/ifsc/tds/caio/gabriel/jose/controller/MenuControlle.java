a6b948f642464b223fc38428c0b63aa01466a484
costinha32 <caio.ic12@aluno.ifsc.edu.br>
2023-06-01 11:24:12 -0300

    Foi criado o menuview e o menucontrolle

---
+++ ProjetoAgenda/src/com/ifsc/tds/caio/gabriel/jose/controller/MenuControlle.java
@@ -0,0 +1,10 @@
+package com.ifsc.tds.caio.gabriel.jose.controller;
+
+public class MenuControlle {
+
+	public boolean onCloseQuery() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
