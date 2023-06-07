40e13855b6753f946e79cd7860fcb16d50c7b275
José Carlos Moura <124727238+zecamoura@users.noreply.github.com>
2023-06-07 12:50:12 -0300

    Update ClientesDAO.java
    criando classe cliente dao

--- ProjetoLocadora/src/com/ifsc/tds/caio/gabriel/jose/dao/ClientesDAO.java
+++ ProjetoLocadora/src/com/ifsc/tds/caio/gabriel/jose/dao/ClientesDAO.java
@@ -1,5 +1,39 @@
 package com.ifsc.tds.caio.gabriel.jose.dao;
 
-public class ClientesDAO {
+import java.util.List;
+
+import com.ifsc.tds.caio.gabriel.jose.enity.Clientes;
+
+public class ClientesDAO implements DAO<Clientes> {
+
+	@Override
+	public Clientes get(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Clientes> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int save(Clientes t) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean update(Clientes t, String[] params) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(Clientes t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
