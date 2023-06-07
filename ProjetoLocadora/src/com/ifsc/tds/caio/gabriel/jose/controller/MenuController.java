18803149545c03f23cb3ca14824116c7b45574d0
Gabriel Souza da Costa <124728028+Gabrielsc19@users.noreply.github.com>
2023-06-07 16:30:34 -0300

    r6iur6isr6i
    sryaetr5u

--- ProjetoLocadora/src/com/ifsc/tds/caio/gabriel/jose/controller/MenuController.java
+++ ProjetoLocadora/src/com/ifsc/tds/caio/gabriel/jose/controller/MenuController.java
@@ -1,5 +1,68 @@
 package com.ifsc.tds.caio.gabriel.jose.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.layout.VBox;
+
 public class MenuController {
+
+    @FXML
+    private VBox pnlPrincipal;
+
+    @FXML
+    private MenuBar barMenu;
+
+    @FXML
+    private Menu mnuCadastro;
+
+    @FXML
+    private MenuItem mnoCliente;
+
+    @FXML
+    private MenuItem mnoFilmes;
+
+    @FXML
+    private MenuItem mnoEmprestimo;
+
+    @FXML
+    private SeparatorMenuItem sepCadastro;
+
+    @FXML
+    private MenuItem mnoSair;
+
+    @FXML
+    private Menu mnuAjuda;
+
+    @FXML
+    private MenuItem mnoSobre;
+
+    @FXML
+    void onClickMnoCliente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickMnoEmprestimo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickMnoFilmes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickMnoSair(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickMnoSobre(ActionEvent event) {
+
+    }
 
 }
