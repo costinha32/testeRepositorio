2a90651401987e68b51c0bf1b54a12fed1f1548a
José Carlos Moura <124727238+zecamoura@users.noreply.github.com>
2023-06-28 15:42:02 -0300

    Update agenda.sql

--- ProjetoAgenda/sql/agenda.sql
+++ ProjetoAgenda/sql/agenda.sql
@@ -1,60 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 06/06/2023 às 16:10
--- Versão do servidor: 10.4.28-MariaDB
--- Versão do PHP: 8.2.4
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `agenda`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `contato`
---
-
-CREATE TABLE `contato` (
-  `idContato` int(11) NOT NULL,
-  `nomeContato` varchar(120) NOT NULL,
-  `telefoneContato` varchar(30) DEFAULT NULL,
-  `emailContato` varchar(120) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices de tabela `contato`
---
-ALTER TABLE `contato`
-  ADD PRIMARY KEY (`idContato`);
-
---
--- AUTO_INCREMENT para tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `contato`
---
-ALTER TABLE `contato`
-  MODIFY `idContato` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+create table contato(
+id int (100)PRIMARY KEY AUTO_INCREMENT not NULL ,
+nome VARCHAR(100) NOT NULL,
+telefone VARCHAR(100) null,
+email VARCHAR(100) NULL
+);
